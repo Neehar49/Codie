8ea60e3ed0c4ddbd4a24c8c556eef9e46c91b9c3
Neehar Tiwari <117676609+Neehar49@users.noreply.github.com>
2024-11-22 15:17:58 +0530

    ten

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,16 +1,14 @@
 import { useState } from "react";
 
-// Function to toggle a class on an element
-export const toggleClass = (el, className) => {
+export const toggleClass = (el,className) => {
   let elem = document.querySelector(el);
   elem.classList.toggle(className);
 };
 
-// Function to remove a class from an element
-export const removeClass = (el, className) => {
+export const removeClass = (el,className) => {
   let elem = document.querySelector(el);
   elem.classList.remove(className);
 };
 
-// Set api_base_url based on environment variable
-export const api_base_url = process.env.REACT_APP_API_BASE_URL || "http://localhost:3000"; // Default to localhost if not set
+
+export const api_base_url = "http://localhost:3000"
